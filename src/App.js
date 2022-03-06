c48e345186d87ca76687dc22393ea631c6de1a0b
anavaleska2908 <avaleska2908@hotmail.com>
2022-03-06 15:47:04 -0300

    fix: error appearing in console about memory

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import { Routes } from './routes';
 import GlobalStyle from './styles/global';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [value, setValue] = useState('checking value...');
+  useEffect(() => {
+    let isMounted = true;
+    fetch().then(() => {
+      if (isMounted) {
+        setValue('done!');
+      }
+    });
+    return () => {
+      isMounted = false;
+    };
+  });
   return (
     <>
       <ToastContainer
